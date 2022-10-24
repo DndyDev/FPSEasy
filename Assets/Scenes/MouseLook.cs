0812b0816b5eb0add985a3d6de45fcb6bdaea88f
Sergey <sirtikhomirov@gmail.com>
2022-10-24 21:03:38 +0300

    add keyboard control and look around

--- Assets/Scenes/MouseLook.cs
+++ Assets/Scenes/MouseLook.cs
@@ -14,23 +14,28 @@ public class MouseLook : MonoBehaviour
 
     public RotationAxes axes = RotationAxes.MouseXAndY;
 
-    public float sesitivityHorizontal = 9.0f;
-    public float sesitivityVertical = 9.0f;
+    public float sensitivityHorizontal = 9.0f;
+    public float sensitivityVertical = 9.0f;
 
     public float minVertical = -45.0f;
     public float maxVertical = 45.0f;
 
     private float _rotationX = 0;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+            body.freezeRotation = true;
+    }
     void Update()
     {
         if (axes == RotationAxes.MouseX)
         {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sesitivityHorizontal, 0);
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHorizontal, 0);
         }
         else if(axes == RotationAxes.MouseY){
-            _rotationX -= Input.GetAxis("Mouse Y") * sesitivityVertical;
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVertical;
             _rotationX = Mathf.Clamp(_rotationX, minVertical, maxVertical);
 
             float rotationY = transform.localEulerAngles.y;
@@ -39,7 +44,15 @@ public class MouseLook : MonoBehaviour
         }
         else
         {
-           
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVertical;
+            _rotationX = Mathf.Clamp(_rotationX, minVertical, maxVertical);
+
+            float delta = Input.GetAxis("Mouse X") * sensitivityHorizontal;
+            float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+
+
         }
     }
 }
