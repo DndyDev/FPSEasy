344d68710042a96f33e9474eca85058d9921a72c
Sergey <sirtikhomirov@gmail.com>
2022-11-01 09:52:10 +0300

    add Enemy and FPS
    Add Enemy witch simple AI and FPS interface, control and shooting

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPSinput")]
 public class FPSInput : MonoBehaviour
 {
     public float speed = 6.0f;
+    public float gravity = -9.8f;
     private CharacterController _characterController;
 
     private void Start()
@@ -17,8 +20,9 @@ public class FPSInput : MonoBehaviour
     {
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
-        Vector3 movement = new Vector3 (deltaX, 0, deltaZ);
+        Vector3 movement = new Vector3 (deltaX, gravity, deltaZ);
         movement = Vector3.ClampMagnitude(movement, speed);
+        //movement.y = gravity;
 
         movement *= Time.deltaTime;
         movement = transform.TransformDirection(movement);
